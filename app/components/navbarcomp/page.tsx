37af6671b36c050b77abb2a547f066e5c0164cc2
neslihan <neslihan_atasvr111@outlook.com>
2024-02-04 18:23:29 +0300

    vercel

--- app/components/navbarcomp/page.tsx
+++ app/components/navbarcomp/page.tsx
@@ -1,64 +1,70 @@
-import {
-  DashboardIcon,
-  ExitIcon,
-  GearIcon,
-} from "@radix-ui/react-icons";
+import { DashboardIcon, ExitIcon, GearIcon } from "@radix-ui/react-icons";
 import { Avatar, Box, Flex, IconButton, Text } from "@radix-ui/themes";
 
 export default function NavbarComponent() {
   return (
     <Box>
       <Flex
-      direction="column"
-      gap="5"
-      justify="between"
-      mt="5"
-      ml="8"
-      style={{ height: "calc(100vh - 130px)"}}
-    >
-      <Flex gap="5" direction="column">
-      <Avatar
-        fallback={"a"}
-        size="4"
-        src="https://avatars.githubusercontent.com/u/34745911?s=200&v=4"
-      ></Avatar>
+        direction="column"
+        gap="5"
+        justify="between"
+        mt="5"
+        ml="8"
+        style={{ height: "calc(100vh - 130px)" }}
+      >
+        <Flex gap="5" direction="column">
+          <Avatar
+            fallback={"a"}
+            size="4"
+            src="https://avatars.githubusercontent.com/u/34745911?s=200&v=4"
+          ></Avatar>
 
-        <Flex align="center" gap="3">
-        <IconButton size="3" color="blue" variant="soft">
-          <DashboardIcon width="18" height="18" />
-        </IconButton>
-        <Text weight="bold" color="gray">Dashboard</Text>
+          <Flex align="center" gap="3">
+            <IconButton size="3" color="blue" variant="soft">
+              <DashboardIcon width="18" height="18" />
+            </IconButton>
+            <Text weight="bold" color="gray">
+              Dashboard
+            </Text>
+          </Flex>
+          <Flex align="center" gap="3">
+            <IconButton size="3" color="blue" variant="soft">
+              <DashboardIcon width="18" height="18" />
+            </IconButton>
+            <Text weight="bold" color="gray">
+              Dashboard
+            </Text>
+          </Flex>
+          <Flex align="center" gap="3">
+            <IconButton size="3" color="blue" variant="soft">
+              <DashboardIcon width="18" height="18" />
+            </IconButton>
+            <Text weight="bold" color="gray">
+              Dashboard
+            </Text>
+          </Flex>
         </Flex>
-        <Flex align="center" gap="3">
-        <IconButton size="3" color="blue" variant="soft">
-          <DashboardIcon width="18" height="18" />
-        </IconButton>
-        <Text weight="bold" color="gray">Dashboard</Text>
-        </Flex>
-        <Flex align="center" gap="3">
-        <IconButton size="3" color="blue" variant="soft">
-          <DashboardIcon width="18" height="18" />
-        </IconButton>
-        <Text weight="bold" color="gray">Dashboard</Text>
+
+        <Flex direction="column" gap="5">
+          <Flex align="center" gap="3">
+            <IconButton size="3" color="red" variant="soft">
+              <GearIcon width="18" height="18" />
+            </IconButton>
+            <Text weight="bold" color="gray">
+              Settings
+            </Text>
+          </Flex>
+
+          <Flex align="center" gap="3">
+            <IconButton size="3" color="orange" variant="soft">
+              <ExitIcon width="18" height="18" />
+            </IconButton>
+            <Text weight="bold" color="gray">
+              SignOut
+            </Text>
+          </Flex>
         </Flex>
       </Flex>
-
-      <Flex direction="column" gap="5">
-        <Flex align="center" gap="3">
-        <IconButton size="3" color="red" variant="soft">
-          <GearIcon width="18" height="18" />
-        </IconButton>
-        <Text weight="bold" color="gray">Settings</Text>
-        </Flex>
-
-        <Flex align="center" gap="3">
-        <IconButton size="3" color="orange" variant="soft">
-          <ExitIcon width="18" height="18" />
-        </IconButton>
-        <Text weight="bold" color="gray">SignOut</Text>
-        </Flex>
-      </Flex>
-    </Flex>
     </Box>
   );
 }
